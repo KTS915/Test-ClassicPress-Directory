394cf2c9eee93d0ddddfc8f84a80639c6cca2697
Tim Kaye <KTS915@users.noreply.github.com>
2022-08-21 16:09:57 -0400

    Update registration-users.php
    Ensure Categories and Tags prefixes show on the appropriate archive pages

--- mu-plugins/registration-users.php
+++ mu-plugins/registration-users.php
@@ -273,13 +273,7 @@ add_action( 'init', 'kts_change_author_base' );
 
 /* REMOVE TITLE PREFIX ON ARCHIVE PAGES */
 function kts_change_author_archive_base( $title ) {
-    if ( is_category() ) {
-        $title = single_cat_title( '', false );
-    }
-    elseif ( is_tag() ) {
-        $title = single_tag_title( '', false );
-    }
-    elseif ( is_author() ) {
+    if ( is_author() ) {
         $title = '<span class="vcard">' . get_the_author() . '</span>';
     }
     elseif ( is_tax() ) {
